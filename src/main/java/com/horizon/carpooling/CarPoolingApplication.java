a63a268edb98489390980123b18f9ddfeca26d17
Houssem HJAIJ <houssem.hjaij@proxym-it.com>
2023-11-28 21:33:02 +0100

    [CREATE] initialize project

--- src/main/java/com/horizon/carpooling/CarPoolingApplication.java
+++ src/main/java/com/horizon/carpooling/CarPoolingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarPoolingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarPoolingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarPoolingApplication.class, args);
+    }
 
 }
