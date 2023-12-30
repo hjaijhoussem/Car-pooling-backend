6dd0bf55689c50293c15e8b0c365ab8409f9463b
Houssem HJAIJ <houssem.hjaij@proxym-it.com>
2023-12-30 15:23:23 +0100

    [ADD] user manage account service

--- src/main/java/com/horizon/carpooling/dao/UserRepository.java
+++ src/main/java/com/horizon/carpooling/dao/UserRepository.java
@@ -2,10 +2,12 @@ package com.horizon.carpooling.dao;
 
 import com.horizon.carpooling.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
+
 }
---
+++ src/main/java/com/horizon/carpooling/services/UserService.java
@@ -0,0 +1,36 @@
+package com.horizon.carpooling.services;
+
+import com.horizon.carpooling.dao.UserRepository;
+import com.horizon.carpooling.dto.user.UserDetailDto;
+import com.horizon.carpooling.dto.user.UserUpdateRequestDto;
+import com.horizon.carpooling.entities.User;
+import com.horizon.carpooling.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userDao;
+    private final ModelMapper mapper;
+    public UserDetailDto getUserInfo(UserDetails userDetails) {
+        User authenticatedUser = userDao.findByEmail(userDetails.getUsername()).orElseThrow(UserNotFoundException::new);
+        return mapper.map(authenticatedUser, UserDetailDto.class);
+    }
+
+    public UserDetailDto updateUserAccount(UserDetails userDetails, UserUpdateRequestDto updatedUser) {
+        User authenticatedUser = userDao.findByEmail(userDetails.getUsername()).orElseThrow(UserNotFoundException::new);
+        authenticatedUser.setEmail(updatedUser.getEmail());
+        authenticatedUser.setFirstname(updatedUser.getFirstname());
+        authenticatedUser.setLastname(updatedUser.getLastname());
+        authenticatedUser.setPhoneNumber(updatedUser.getPhoneNumber());
+        return mapper.map(userDao.save(authenticatedUser), UserDetailDto.class);
+    }
+
+    public void deleteAccount(UserDetails userDetails) {
+        User authenticatedUser = userDao.findByEmail(userDetails.getUsername()).orElseThrow(UserNotFoundException::new);
+        userDao.deleteById(authenticatedUser.getId());
+    }
+}
